4058d24a630d230173142c6ea0b01370f842fc9e
Juha Siponen <juha.siponen@vincit.fi>
2022-05-12 13:50:03 +0300

    Change dice view layout

--- src/dice/Dice.tsx
+++ src/dice/Dice.tsx
@@ -4,52 +4,93 @@ import {ToggleButton} from "../common/CommonInput";
 
 type ThrowMode = 'FAST' | 'POWER';
 
+interface Dice {
+    name: string;
+    min: number;
+    max: number;
+}
+
+const DICE: Dice[] = [
+    {name: "D4", min: 1, max: 4},
+    {name: "D6", min: 1, max: 6},
+    {name: "D8", min: 1, max: 8},
+    {name: "D10 (0-9)", min: 0, max: 9},
+    {name: "D10 (1-10)", min: 1, max: 10},
+    {name: "D12", min: 1, max: 12},
+    {name: "D20", min: 1, max: 20},
+];
+
+type DiceResult = number|null;
+
 export const Dice = () => {
 
-    const [diceResults, setDiceResults] = useState<Array<number>>([0]);
+    const [diceResults, setDiceResults] = useState<Array<DiceResult>>([0]);
     const [numOfDice, setNumOfDice] = useState(1);
+    const [minResult, setMinResult] = useState(1);
     const [maxResult, setMaxResult] = useState(6);
     const [throwMode, setThrowMode] = useState<ThrowMode>('FAST');
 
     const changeNumOfDice = (newValue: number) => {
         setNumOfDice(newValue);
-        const results: number[] = [];
+        const results: DiceResult[] = [];
         for (let i = 0; i < newValue; ++i) {
-            results.push(0);
+            results.push(null);
         }
         setDiceResults(results);
     }
 
-    const getDieButton = (buttonResult: number) => {
+    const getDieButton = (die: Dice) => {
+        const isSelected = maxResult === die.max && minResult === die.min;
         return <button
-            className={"button" + (maxResult === buttonResult ? ' is-active is-info' : '')}
-            onClick={(e) => setMaxResult(buttonResult)}
+            className={"button" + (isSelected ? ' is-active is-info' : '')}
+            onClick={(e) => {
+                setMinResult(die.min);
+                setMaxResult(die.max);
+            }}
         >
-            {'D' + buttonResult}
+            {die.name}
         </button>
     }
 
-    const getResults = (results: number[]) => {
+    const getResults = (results: DiceResult[]) => {
         return Array.from(results)
             .map(result => {
                 return (
                     <div className={"dice-result"}>
-                        {result > 0 ? result : '-'}
+                        {result !== null ? result : '-'}
                     </div>
                 );
             });
+    }
+
+    const getRandomValue = (min: number, max: number): number => {
+        return Math.floor(Math.random() * (max-min+1) + min);
+    }
+
+    const getDieStats = (results: DiceResult[]) => {
+        const stats: { [key: number]: number } = {};
+        results.forEach(result => {
+            if (result !== null) {
+                const v = stats[result] || 0;
+                stats[result] = v + 1;
+            }
+        })
+
+        return Object.entries(stats).map(([k, v]) => {
+            return <div>{k}: {v}</div>
+        });
     }
 
     const throwDice = () => {
         if (throwMode === 'FAST') {
             const results = Array.from(diceResults);
             for (let i = 0; i < diceResults.length; ++i) {
-                results[i] = Math.ceil(Math.random() * maxResult);
+                results[i] = getRandomValue(minResult, maxResult);
             }
             setDiceResults(results);
         } else {
             const maxCounter = 20;
-            const results = Array.from(diceResults).map(_ => 0);
+            const results = Array.from(diceResults).map(_ => null as number|null);
             const maxLength = diceResults.length;
             const v = {index: 0, counter: maxCounter};
             const interval = setInterval(function() {
@@ -58,7 +99,9 @@ export const Dice = () => {
                     return;
                 }
 
-                results[v.index] = Math.ceil(Math.random() * maxResult);
+                if (v.index !== null) {
+                    results[v.index] = getRandomValue(minResult, maxResult);
+                }
                 setDiceResults(Array.from(results));
                 v.counter--;
 
@@ -70,32 +113,66 @@ export const Dice = () => {
         }
     }
 
+    const getDiceButtons = () => {
+        return DICE.map(die => getDieButton(die));
+    }
+
     return (
         <div className={"container dice-container"}>
             <div className={"buttons is-centered"}>
                 <label className={"label"}>Dice Type</label>
-                {getDieButton(4)}
-                {getDieButton(6)}
-                {getDieButton(8)}
-                {getDieButton(10)}
-                {getDieButton(12)}
-                {getDieButton(20)}
+                {getDiceButtons()}
             </div>
 
-            <div className={"field"}>
-                <label className={"label"}>Number of Dice</label>
-                <p className={"control"}>
-                    <input
-                        type={"number"}
-                        className={"input dice-number"}
-                        name={"number-of-dice"}
-                        value={numOfDice}
-                        min={1}
-                        onChange={(e) => changeNumOfDice(parseInt(e.target.value))}
-                    />
-                </p>
+            <div className={"block dice-options is-centered"}>
+                <div className="block field is-horizontal is-centered">
+                    <div className="field-label is-normal">
+                        <label className={"label"}>Range</label>
+                    </div>
+                    <div className="field-body">
+                        <div className="field">
+                            <p className={"control"}>
+                                <input
+                                    type={"number"}
+                                    className={"input dice-number"}
+                                    name={"dice-min"}
+                                    value={minResult}
+                                    onChange={(e) => setMinResult(parseInt(e.target.value))}
+                                />
+                            </p>
+                        </div>
+
+                        <div className="field">
+                            <p className={"control"}>
+                                <input
+                                    type={"number"}
+                                    className={"input dice-number"}
+                                    name={"dice-max"}
+                                    value={maxResult}
+                                    onChange={(e) => setMaxResult(parseInt(e.target.value))}
+                                />
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className={"block is-centered"}>
+                    <label className={"label"}>Number of Dice</label>
+                    <p className={"control"}>
+                        <input
+                            type={"number"}
+                            className={"input dice-number"}
+                            name={"number-of-dice"}
+                            value={numOfDice}
+                            min={1}
+                            onChange={(e) => changeNumOfDice(parseInt(e.target.value))}
+                        />
+                    </p>
+                </div>
             </div>
-            <div className={"is-centered"}>
+
+            <div className={"block"}/>
+
+            <div className={"block is-centered"}>
                 <div className={"buttons is-centered"}>
                     <ToggleButton currentValue={throwMode} value={'FAST'} onClick={setThrowMode}>Fast</ToggleButton>
                     <ToggleButton currentValue={throwMode} value={'POWER'} onClick={setThrowMode}>Power</ToggleButton>
